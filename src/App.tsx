d7ea3dd5d258f8658a65fc27909ab32d134000dd
senyameow <arseniyxd@gmail.com>
2024-02-18 23:51:54 +0700

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Counter } from './components/Counter'
+import './index.scss'
+import { Route, Router, Routes } from 'react-router-dom'
+import AboutPage from './pages/AboutPage/AboutPage'
+import MainPage from './pages/MainPage/MainPage'
+import { Link } from 'react-router-dom'
 import './index.scss'
 
 const App = () => {
     return (
         <div className='app'>
-            qweqwe
-            <Counter />
+            <Link to={'/'} >Main page</Link>
+            <Link to={'/about'} >About page</Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage />} />
+                <Route path={'/'} element={<MainPage />} />
+            </Routes>
         </div>
     )
 }
