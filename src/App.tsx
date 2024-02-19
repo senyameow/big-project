4ad03c3d1c84df1f18739eb88ac4f795456e7143
senyameow <arseniyxd@gmail.com>
2024-02-20 00:06:55 +0700

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import './index.scss'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Router, Routes, Link } from 'react-router-dom'
 import AboutPage from './pages/AboutPage/AboutPage'
 import MainPage from './pages/MainPage/MainPage'
-import { Link } from 'react-router-dom'
 import './index.scss'
 
 const App = () => {
     return (
+
         <div className='app'>
-            <Link to={'/'} >Main page</Link>
-            <Link to={'/about'} >About page</Link>
+            qwe
+            <Link to={'/'} />
             <Routes>
                 <Route path={'/about'} element={<AboutPage />} />
                 <Route path={'/'} element={<MainPage />} />
             </Routes>
         </div>
+
     )
 }
 
