2cc734cdb487c42d46f89db29951da9ae3532855
senyameow <arseniyxd@gmail.com>
2024-02-29 00:17:52 +0700

    i18

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,15 +1,18 @@
 import HtmlWebpackPlugin from "html-webpack-plugin"
-import { ProgressPlugin, WebpackPluginInstance } from "webpack"
+import { DefinePlugin, ProgressPlugin, WebpackPluginInstance } from "webpack"
 import { BuildOptions } from "./types/config"
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
-export const buildPlugins = ({ paths }: BuildOptions): WebpackPluginInstance[] => {
+export const buildPlugins = ({ paths, mode }: BuildOptions): WebpackPluginInstance[] => {
     return [
         new HtmlWebpackPlugin({ template: paths.html }),
         new ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new DefinePlugin({
+            PRODUCTION: JSON.stringify(mode === 'production' ? true : false)
         })
     ]
 }
--- src/app/types/typings.d.ts
+++ src/app/types/typings.d.ts
@@ -9,4 +9,6 @@ declare module "\*.svg" {
     import React = require("react");
     const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
     export default ReactComponent;
-}  
+}
+
+declare const PRODUCTION: boolean
