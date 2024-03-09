b2ec5e2dee8c8d588a7f7aee02c9979a4f993541
senyameow <arseniyxd@gmail.com>
2024-03-09 23:54:13 +0700

    1

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -8,7 +8,7 @@ export const Loader = ({
 }: LoaderProps) => {
     return (
         <div className="loadingio-spinner-bars-hqc1la943ki"><div className="ldio-685d7cu3csk">
-                <div></div><div></div><div></div><div></div>
+            <div></div><div></div><div></div><div>q</div>
         </div></div>
     )
 }
