9b193531276f803f010f020000167f89b3b01b16
senyameow <arseniyxd@gmail.com>
2024-03-10 23:49:20 +0700

    loader

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -1,11 +1,7 @@
 import './Loader.scss'
-interface LoaderProps {
-    className?: string
-}
 
 export const Loader = ({
-    className,
-}: LoaderProps) => {
+}) => {
     return (
         <div className="loadingio-spinner-bars-hqc1la943ki"><div className="ldio-685d7cu3csk">
             <div></div><div></div><div></div><div></div>
