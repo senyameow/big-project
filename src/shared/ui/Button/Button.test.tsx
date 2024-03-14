b77bf82fd2fdef6ec5b47205fca8ae76b4b2f80e
senyameow <arseniyxd@gmail.com>
2024-03-15 00:28:58 +0700

    sidebar test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -3,7 +3,12 @@ import { Button, ButtonTheme } from './Button'
 
 describe('button', () => {
     test('render', () => {
-        render(<Button theme={ButtonTheme.CLEAR}>test</Button>);
+        render(<Button>test</Button>);
         expect(screen.getByText('test')).toBeInTheDocument();
-    })
+    });
+    
+    test('render', () => {
+        render(<Button theme={ButtonTheme.CLEAR}>clear</Button>);
+        expect(screen.getByText('clear')).toHaveClass('clear')
+    });
 })
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -8,7 +8,7 @@ export enum ButtonTheme {
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string
-  theme: ButtonTheme
+  theme?: ButtonTheme
 }
 
 export const Button: FC<ButtonProps> = ({
